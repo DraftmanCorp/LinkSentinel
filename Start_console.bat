125d4f6aee150bf8b232228836282283415e9bd5
DraftmanCorp <62466956+DraftmanCorp@users.noreply.github.com>
2025-06-23 13:55:20 +0200

    Add files via upload

---
+++ Start_console.bat
@@ -0,0 +1,4 @@
+@echo off
+powershell -Command ^
+  "Start-Process -FilePath 'powershell.exe' -ArgumentList '-NoProfile -ExecutionPolicy Bypass -File \"Script.ps1\"' -PassThru | ForEach-Object { $_.Id } | Out-File -Encoding ASCII 'monitor.pid'"
+exit
