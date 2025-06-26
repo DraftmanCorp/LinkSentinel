84f795d668a0946d2a9ad4af011c5ea8c48a84a1
DraftmanCorp <62466956+DraftmanCorp@users.noreply.github.com>
2025-06-26 11:53:31 +0200

    Delete Start_console.bat

--- Start_console.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-powershell -Command ^
-  "Start-Process -FilePath 'powershell.exe' -ArgumentList '-NoProfile -ExecutionPolicy Bypass -File \"Script.ps1\"' -PassThru | ForEach-Object { $_.Id } | Out-File -Encoding ASCII 'monitor.pid'"
-exit
