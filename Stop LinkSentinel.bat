06654c71ebf6c14d89d970984ebf73e792779663
DraftmanCorp <62466956+DraftmanCorp@users.noreply.github.com>
2025-06-26 11:53:15 +0200

    Delete Stop LinkSentinel.bat

--- Stop LinkSentinel.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-set pidFile=monitor.pid
-if exist "%pidFile%" (
-    for /f %%p in (%pidFile%) do taskkill /PID %%p /F
-    del "%pidFile%"
-) else (
-    echo File PID non trovato. Lo script potrebbe non essere in esecuzione.
-)
-exit
